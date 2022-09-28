f6ed5f2365883be7659a01e4de990e0b43a01452
WhaterFalls <jasonhwa@gmail.com>
2022-09-28 15:41:30 -0400

    Added title screen with high score and sync

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameControl : MonoBehaviour {
 
 	public static GameControl instance;
 	public GameObject gameOverText;
 	public Text scoreText;
-	public Text highScoreText;
+	TMP_Text highScoreText;
 	public bool gameOver = false;
 	private int score = 0;
 
@@ -17,6 +18,8 @@ public class GameControl : MonoBehaviour {
 	void Awake () {
 		if (instance == null) {
 			instance = this;
+			highScoreText = GameObject.Find("HighScoreText").GetComponent<TMP_Text>();
+			highScoreText.text = $"High Score: {SessionManager.Instance.HighScore}";
 		} else if (instance != this) {
 			Destroy (gameObject);
 		}
@@ -24,7 +27,7 @@ public class GameControl : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		highScoreText.text = "High Score: " + HighScore ().ToString ();
+		CheckForHighScore();
 		if (gameOver) {
 			StartCoroutine ("Reset");
 		}
@@ -52,14 +55,11 @@ public class GameControl : MonoBehaviour {
 		gameOver = true;
 	}
 
-	int HighScore() {
-		if (PlayerPrefs.HasKey ("High Score")) {
-			if (score > PlayerPrefs.GetInt("High Score")) {
-				PlayerPrefs.SetInt ("High Score", score);
-			}
-		} else {
-			PlayerPrefs.SetInt ("High Score", score);
+	void CheckForHighScore() {
+		if (score > SessionManager.Instance.HighScore)
+        {
+			SessionManager.Instance.SetHighScore(score);
+			highScoreText.text = $"High Score: {SessionManager.Instance.HighScore}";
 		}
-		return PlayerPrefs.GetInt("High Score");
 	}
 }
---
+++ Assets/Scripts/SessionManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SessionManager : MonoBehaviour
+{
+
+    // Create one instance and don't destroy between scenes
+    public static SessionManager Instance;
+
+    #region variables
+    string playerName;
+    TMP_InputField nameField;
+    TMP_Text highScoreText;
+    
+    #endregion
+
+    #region accessors
+    public string PlayerName { get; }
+    public string HighScorePlayer { get { return PlayerPrefs.GetString("High Score Player"); } }
+
+    public int HighScore { get { return PlayerPrefs.GetInt("High Score"); } }
+    #endregion
+
+    private void Awake()
+    {
+        highScoreText = GameObject.Find("HighScoreText").GetComponent<TMP_Text>();
+        highScoreText.text = $"High Score: {HighScore}\nBy: {HighScorePlayer}";
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        nameField = GameObject.Find("NameField").GetComponent<TMP_InputField>();
+        nameField.onEndEdit.AddListener(SetPlayerName);
+    }
+
+    public void StartGame()
+    {
+        if (nameField.text != "")
+        {
+            SceneManager.LoadScene("Game");
+        }
+        else
+        {
+            Debug.Log("Please enter an alias before starting.");
+        }
+    }
+
+    public void SetPlayerName(string name)
+    {
+        playerName = name;
+    }
+
+    public void SetHighScore(int score)
+    {
+        if (PlayerPrefs.HasKey("High Score"))
+        {
+            if (score > PlayerPrefs.GetInt("High Score"))
+            {
+                PlayerPrefs.SetInt("High Score", score);
+                PlayerPrefs.SetString("High Score Player", playerName);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("High Score", score);
+            PlayerPrefs.SetString("High Score Player", playerName);
+        }
+    }
+
+    public void ClearHighScore()
+    {
+        PlayerPrefs.SetInt("High Score", 0);
+        PlayerPrefs.SetString("High Score Player", "");
+        highScoreText.text = "High Score: 0\nBy: ";
+    }
+}
